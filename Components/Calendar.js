ccee76093cd2f7cc0eedd9507facb54f65d9c5c2
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-05 12:57:07 -0800

    Add Calendar Agenda function

--- Components/Calendar.js
+++ Components/Calendar.js
@@ -1,16 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Card, Avatar } from 'react-naitve-paper';
 
-const Calendar = () => {
-  return (
-    <View style={styles.body}>
-        <LinearGradient colors={['#FFF700', '#02EBAF', '#02EBAF', '#FFF700']} style={styles.linearGradient}>
-        <Text style={styles.footerText}>Calendar</Text>
-        </LinearGradient>
-    </View>
-  );
+const timeToString = (time) => {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+}
+
+const Scheduler = () => {
+    const [items, setItems] = useState({});
+
+    const loadItems = (day) => {
+        setTimeout(() => {
+            for (let i = -15; i < 85; i++) {
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+                const strTime = timeToString(time);
+                if (!items[strTime]) {
+                items[strTime] = [];
+                const numItems = Math.floor(Math.random() * 3 + 1);
+                for (let j = 0; j < numItems; j++) {
+                    items[strTime].push({
+                    name: 'Item for ' + strTime + ' #' + j,
+                    height: Math.max(50, Math.floor(Math.random() * 150))
+                    });
+                }
+            }
+        }
+        const newItems = {};
+        Object.keys(items).forEach(key => {
+            newItems[key] = items[key];
+        });
+        setItems(newItems)
+        }, 1000);
+    };
+
+    const renderItem = (item) => {
+        
+    };
+
+    return (
+        <View style={styles.body}>
+            <LinearGradient colors={['#FFF700', '#02EBAF', '#02EBAF', '#FFF700']} style={styles.linearGradient}>
+                <Agenda
+                    items={items}
+                    loadItemsForMonth={loadItems}
+                    selected={'2017-05-16'} 
+                    renderItem={renderItem}
+                />
+            </LinearGradient>
+        </View>
+    );
 }
 
 
@@ -34,5 +75,5 @@ const styles = StyleSheet.create({
     },
 });
 
-export default Calendar;
+export default Scheduler;
 
